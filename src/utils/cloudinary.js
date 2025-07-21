71ca50ccea9a60c6f7314de359f09b94a947860c
Tejveer008 <tejveer.sf@gmail.com>
2025-07-21 21:07:52 +0530

    cloudinary , multer and middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import v2 from "cloudinary";
+import fs from "fs";
+
+cloudnary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary= async(localFilePath)=>{
+    try {
+        if(!localFilePath) return null;
+        //uploading file to cloudinary
+       const response = await cloudinary.v2.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        console.log("file uploaded successfully",response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); //delete the file from local storage
+        console.error("Error uploading file to Cloudinary:", error);
+        return null;
+    }
+} 
