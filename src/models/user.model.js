ee2cb61d5c3bf0bb9e4232d4a7b9050f81331076
Tejveer008 <tejveer.sf@gmail.com>
2025-07-21 11:03:26 +0530

    password hasing

---
+++ src/models/user.model.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs"; 
+import jsonwebtoken from "jsonwebtoken";
+
+
+const userSchema = new mongoose.Schema(
+    {
+        username:{
+            type:String,
+            require:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true
+        },
+        email:{
+            type:String,
+            require:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+        },
+        fullname:{
+            type:String,
+            require:true,
+            trim:true,
+            index:true
+        },
+        avatar:{
+            type:String, //cloudinary url
+            require:true,
+
+        },
+        coverImg:{
+            type:String //cloudinary url
+        },
+        watchHistory:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:'Video'
+            }
+        ],
+        password:{
+            type:String,
+            required:[true,'Password is required']
+        },
+        refreshToken:{
+            type:String
+        }
+    },
+    { timestamps: true });
+
+    //password hashing middleware
+    userSchema.pre("save", async function (next) {
+        if (!this.isModified("password")) return next();
+
+        this.password = await bcrypt.hash(this.password, 10);
+        next();
+    })
+    //password comparison method
+    userSchema.methods.isPasswordCorrect = async function (password) {
+        return await bcrypt.compare(password, this.password);
+    }
+
+export const User = mongoose.model("User", userSchema);
